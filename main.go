cf6bc1b83a5e9c817c2bcbbfae189b6f4449430e
Fakhry-alta <fakhry.ikhsan.firdaus@gmail.com>
2022-08-22 15:49:04 +0700

    resolve conflict

--- main.go
+++ main.go
@@ -4,9 +4,6 @@ import "fmt"
 
 func main() {
 	fmt.Println("menu register sudah jadi")
-<<<<<<< HEAD
 	fmt.Println("menu update data user")
-=======
 	fmt.Println("menu login")
->>>>>>> 92fe52aac71522cbae9e1b90ec85470d256ec0e5
 }
